e5bea1d1ae8d00591d2df2b53a4b63bf0424548a
EstrellaDionis <EstrellaDionis@gmail.com>
2022-06-27 14:53:11 -0400

    added comments for clarity

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import AddUser from './Users/AddUser';
 import UsersList from './Users/UsersList';
 
 function App() {
+  //useState is empty here because the list is empty
   const [usersList, setUsersList] = useState([])
 
   const addUserHandler = (userName, userAge) => {
+    //always want to pass a function to setUsersList because we want to grab the previous state to update
     setUsersList((prevUserList) =>{
       //returning an array because thats what we want and is our state. An array of users
       return [...prevUserList, {name: userName, age: userAge, id: Math.random().toString()}]
